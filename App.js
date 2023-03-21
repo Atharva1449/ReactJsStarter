5b380a1eabec1dab1d72b425dee887d601a2e6d1
atharva_dawande <atharva.dawande603@gmail.com>
2023-03-21 23:07:23 +0530

    initial

---
+++ App.js
@@ -0,0 +1,10 @@
+const heading = React.createElement("h1", { id: "yooo" }, "hello"); //so the id and other attribute we did in html we pass here in reeact
+const heading2 = React.createElement("h2", { id: "yooo2" }, "hello2"); //so the id and other attribute we did in html we pass here in reeact
+const container = React.createElement("div", { id: "container" }, [
+  heading,
+  heading2,
+]); //here as it is div tag we are passing h1 and h2 tag but as there are two tags we pass in form of array
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+console.log(heading); //actually heading is an object if you check in consolelog
+root.render(container);
