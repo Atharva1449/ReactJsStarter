72f039ec0f9c785598414f7485a98e522150a4e8
atharva_dawande <atharva.dawande603@gmail.com>
2023-05-29 23:32:51 +0530

    initial app layout

--- App.js
+++ App.js
@@ -1,32 +1,44 @@
 import React from "react";
-import  ReactDOM  from "react-dom/client";
-//below is react element
-const title=(
-  <h1 className="h1class">Hello</h1>
-);
-
-//below is react function component
-const Title2=()=>{
-return (
-  <h2 className="h2class">Hello2</h2>
-);
-}
-
-// <Title2/>   this is component composition
-//{} in this you can pass any code so the above same line of code can be also printed
-//using {Title2()} now why this works coz functional component is basic js function
-const Container=()=>{
+import ReactDOM from "react-dom/client";
+const  root=ReactDOM.createRoot(document.getElementById("root"));
+const Title=()=>{
   return(
-    <div>
-      {title}
-      {2-3}
-      <Title2/> 
-      {Title2()} 
-      <h3>hello3</h3>
-    </div>
+    <h1>Food Tu amas</h1>
   );
 }
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-//console.log(heading); //actually heading is an object if you check in consolelog
-root.render(<Container></Container>);
+const Header=()=>{
+  return (
+    <div >
+      <Title></Title>
+      <div>
+        <ul>
+          <li>Home</li>
+          <li>About</li>
+          <li>Cart</li>
+        </ul>
+      </div>
+    </div>
+  );
+}
+const Body=()=>{
+  return (
+<div>body</div>
+  );
+}
+const Footer=()=>{
+  return (
+    <div>footer</div>
+  );
+}
+
+const AppLayout=()=>{
+  return(
+    <>
+    <Header></Header>
+    <Body></Body>
+    <Footer></Footer>
+    </>
+  );
+}
+root.render(<AppLayout></AppLayout>);
