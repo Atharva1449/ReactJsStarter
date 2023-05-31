d366b1a2b7c172a7d84aa45a8fc1ad793ca8932b
atharva_dawande <atharva.dawande603@gmail.com>
2023-05-31 10:52:25 +0530

    added styling

--- App.js
+++ App.js
@@ -3,18 +3,19 @@ import ReactDOM from "react-dom/client";
 const  root=ReactDOM.createRoot(document.getElementById("root"));
 const Title=()=>{
   return(
-    <h1>Food Tu amas</h1>
+    <img className="logo" alt="logo" src="https://media.istockphoto.com/id/1208194075/vector/local-craft-food-design-template-local-food-poster-for-restaurant-cooking-and-food-business.jpg?s=612x612&w=0&k=20&c=uIhhmIz_yBy2u-1urD8Dm0bxIt5D30aAgDEr0vvJShk=" alt="food image"></img>
   );
 }
 
 const Header=()=>{
   return (
-    <div >
+    <div className="navbar" >
       <Title></Title>
-      <div>
+      <div className="listElements">
         <ul>
           <li>Home</li>
           <li>About</li>
+          <li>Contact</li>
           <li>Cart</li>
         </ul>
       </div>
