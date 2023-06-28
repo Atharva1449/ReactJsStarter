217db793516cc947a6ec0cf60bc40893beefaee9
atharva_dawande <atharva.dawande603@gmail.com>
2023-06-28 11:31:20 +0530

    Added search functionality

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,12 +1,34 @@
 import { CardTemplate } from "./Card.js";
 import { restaurantList } from "../constants.js"
+import { useState } from "react";
+function filterSearchtext(searchTextValueParam,restaurantListDataParam){
+    return restaurantList.filter((restaurantListDataParam2)=>
+    ( restaurantListDataParam2.data.name.includes(searchTextValueParam)));
+   }
+   
 const Body=()=>{
+    const [restaurantListData,setRestaurantListData]=useState(restaurantList);
+    const [searchTextValue,setSearchTextValue]=useState();
+  
     return (
+        <>
+        <input
+        type="text"
+        placeholder="Type search text"
+        value={searchTextValue}
+        onChange={(e)=>{setSearchTextValue(e.target.value);}}
+        ></input>
+        <button
+        onClick={ ()=>{
+        setRestaurantListData(filterSearchtext(searchTextValue,restaurantListData))}
+        }
+        >Search for {searchTextValue}
+        </button>
       <div className="restaurantCardsList">
-        { restaurantList.map((restaurantList)=>{return <CardTemplate {...restaurantList.data} key={restaurantList.data.id}/>})}
+        { restaurantListData.map((restaurantListsparam)=>{return <CardTemplate {...restaurantListsparam.data} key={restaurantListsparam.data.id}/>})}
         
      
-  </div>
+  </div></>
     );
   }
 
